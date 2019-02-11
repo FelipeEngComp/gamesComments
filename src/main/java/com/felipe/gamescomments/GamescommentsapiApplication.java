0c8a17149fb54ec20b0c73ee466668b05c1bed40
felipe <felipe.rosa@sysmap.com.br>
2019-02-11 20:32:49 -0200

    Criacao do projeto

---
+++ src/main/java/com/felipe/gamescomments/GamescommentsapiApplication.java
@@ -0,0 +1,14 @@
+package com.felipe.gamescomments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamescommentsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamescommentsapiApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/felipe/gamescomments/GamescommentsapiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.felipe.gamescomments;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GamescommentsapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
